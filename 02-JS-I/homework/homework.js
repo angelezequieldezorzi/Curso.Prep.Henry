14653556266223a8570b9d200afa4031f1f21112
unknown <angelezequieldezorzi@gmail.com>
2022-04-22 01:22:11 -0300

    1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -194,7 +194,8 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
- 
+  var saludo = ("Hola"+ nombre + "!");
+  return saludo;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
